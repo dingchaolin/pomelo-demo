3a4599dafc1a4a705677b379a1f2bd4742e8f521
User-PC\User <dingchaolin@slhuyu.com>
2017-06-28 21:20:43 +0800

    feat:date test

--- date/test.js
+++ date/test.js
@@ -13,3 +13,6 @@ let time1 = `2017-06-27`;
 if( time1 != time ){
     console.log( + new Date(time), + new Date(time1) , (+ new Date(time) < + new Date(time1)) )
 }
+
+let timeStr = new Date(+new Date(time1)).toLocaleString();
+console.log( timeStr );//2017-6-27 08:00:00
